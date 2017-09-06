769a7184bbcbcd209a028ad7bd34f2af8a3057b6
Jaziel Moreira <Jaziel Moreira@DESKTOP-L1BG4O0>
2017-09-05 23:54:34 -0300

    Signed-off-by: Jaziel Moreira <Jaziel Moreira@DESKTOP-L1BG4O0>

--- src/main/Purity.java
+++ src/main/Purity.java
@@ -5,6 +5,13 @@ package main;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
+
 
 
 public class Purity {
@@ -29,34 +36,53 @@ public class Purity {
 		
 		GithubDownloader git=new GithubDownloader(urlRepository);
 		
-		git.setLocation("/tmp/Projeto/Downloads/"+commit);
+		git.setLocation("c:/tmp/Projeto/Downloads/"+commit);
+		
+
+		File sourceFile=git.downloadCommit(parent);
+		File sourceFolder=ZipExtractor.extract(sourceFile, new File(git.getLocation(),parent));
+		
+		System.out.println(sourceFolder.getAbsolutePath());
+		InvocationRequest request = new DefaultInvocationRequest();
+		request.setPomFile( new File( sourceFolder,"pom.xml" ) );
+		request.setGoals( Arrays.asList( "compile" ) );	
+		
+		Invoker invoker = new DefaultInvoker();
+		invoker.setMavenHome(new File("C:\\Program Files\\apache-maven-3.5.0"));
+		invoker.execute( request );
+		
+	
+		
+		
+		/*Process p=Runtime.getRuntime().exec("mvn -f "+sourceFolder.getAbsolutePath()+" clean");
+		BufferedReader reader =
+				new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String s;
+		while ((s=reader.readLine()) != null) {System.out.println(s);}
+		p.waitFor();
+		System.out.println("Estatus da compilação: "+p.exitValue());*/
+		
 		
 		
 		File targetFile=git.downloadCommit(commit);
 		File targetFolder=ZipExtractor.extract(targetFile, new File(git.getLocation(),commit));
 		
+		System.out.println(targetFolder.getAbsolutePath());
+		request = new DefaultInvocationRequest();
+		request.setPomFile( new File( sourceFolder,"pom.xml" ) );
+		request.setGoals( Arrays.asList( "compile" ) );	
 		
-		File sourceFile=git.downloadCommit(parent);
-		File sourceFolder=ZipExtractor.extract(sourceFile, new File(git.getLocation(),parent));
+		invoker.execute(request);
 		
-		
-		
-		System.out.println("Copiling: "+sourceFolder);
-		Process p=Runtime.getRuntime().exec("cmd /C mvn -f "+sourceFolder.getAbsolutePath()+" compile");
-		BufferedReader reader =
-				new BufferedReader(new InputStreamReader(p.getInputStream()));
-		while ((reader.readLine()) != null) {}
-		p.waitFor();
-		System.out.println("Estatus da compilação: "+p.exitValue());
-		
+		/*
 		System.out.println("Copiling: "+targetFolder);
-		p=Runtime.getRuntime().exec("cmd /C mvn -f "+targetFolder.getAbsolutePath()+" compile");
+		p=Runtime.getRuntime().exec("mvn -f "+targetFolder.getAbsolutePath()+" compile");
 		reader =
 				new BufferedReader(new InputStreamReader(p.getInputStream()));
 		while ((reader.readLine()) != null) {}
 		p.waitFor();
 		System.out.println("Estatus da compilação: "+p.exitValue());
-		
+		*/
 		return 0;
 	}
 
